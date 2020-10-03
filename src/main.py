c4e7a70a5d8fc52bfbba16f564eeb3a260171919
mjc862 <mjc862@usask.ca>
2020-10-02 20:42:08 -0600

    Updated request url from {project_name} to projects/{project} name

--- src/main.py
+++ src/main.py
@@ -39,14 +39,14 @@ async def post_doc(index_name: str, doc: NewDoc):
     except Exception as e:
         return str(e)
 
-@app.put("/{index_name}/")
+@app.put("/projects/{index_name}/")
 async def put_doc(index_name: str, request_body: dict):
     try:
         return await es.indices.create(index_name, request_body)
     except Exception as e:
         return str(e)
 
-@app.post("/{project_name}/")
+@app.post("/projects/{project_name}/")
 async def post_doc(project_name: str, new_event: Event):
     try:
         return await es.index(project_name, new_event.dict())
