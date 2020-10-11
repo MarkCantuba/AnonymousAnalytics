3959560c6a982abddfa6454c946c1cad76974fa3
Xiaoyan Xu <xuxiaoyansean@gmail.com>
2020-10-10 20:11:47 -0600

    In get Events part, add time zone validation for start&end value
    raise HTTP error 500 if start&end value has no timezone info or are not
    in UTC timezone

--- src/main.py
+++ src/main.py
@@ -5,7 +5,12 @@ from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 
 import config
+<<<<<<< HEAD
 from exceptions import *
+=======
+import pytz
+
+>>>>>>> b386624... In get Events part, add time zone validation for start&end value
 from models import *
 
 conf = config.get()
@@ -47,6 +52,16 @@ async def post_event_to_project(project_name: str, event: dict):
 async def get_events_by_timestamp(project_name: str,
                                   start: datetime = datetime.now(timezone.utc) - timedelta(days=7),
                                   end: datetime = datetime.now(timezone.utc)):
+    # validate start & end when they are not given as default value
+    if start.tzinfo == None:
+        raise HTTPException(status_code=500, detail='Error: the given datetime: {} has no timezone information'.format(start))
+    if end.tzinfo == None:
+        raise HTTPException(status_code=500, detail='Error: the given datetime: {} has no timezone information'.format(end))
+    if not start.tzinfo == pytz.utc:
+        raise HTTPException(status_code=500, detail='Error: timezone of the given datetime: {} is not UTC'.format(start))
+    if not end.tzinfo == pytz.utc:
+        raise HTTPException(status_code=500, detail='Error: timezone if the given datetime: {} is not UTC'.format(end))
+
     try:
         request_body = {
             "query": {
