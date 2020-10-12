16e5f9eea0adc650c9e26db3be06fdec808c0035
mjc862 <mjc862@usask.ca>
2020-10-11 23:37:30 -0600

    Added Post Project and Post Events request along with custom exception checks for index existence.

--- src/main.py
+++ src/main.py
@@ -12,37 +12,13 @@ from exceptions import *
 
 from models import *
 
+
 conf = config.get()
 app = FastAPI()
 es = AsyncElasticsearch([
-    { 'host': conf['elasticsearch']['private_ip'], 'port': conf['elasticsearch']['rest-port'] }
+    {'host': conf['elasticsearch']['private_ip'],
+     'port': conf['elasticsearch']['rest-port'] }
 ])
-
-@app.get("/info")
-async def info():
-    return await es.info()
-
-@app.get("/index/{index_name}")
-async def get_index(index_name: str):
-    try:
-        return await es.search(index=index_name)
-    except Exception as e:
-        return str(e)
-
-@app.get("/index/{index_name}/{doc_id}")
-async def get_doc(index_name: str, doc_id: str):
-    try:
-        return await es.get(index_name, doc_id)
-    except Exception as e:
-        return str(e)
-
-# @app.post("/index/{index_name}")
-# async def post_doc(index_name: str, doc: NewDoc):
-#     try:
-#         return await es.index(index_name, doc.content)
-#     except Exception as e:
-#         return str(e)
-
 
 
 @app.post("/projects")
--- src/models.py
+++ src/models.py
@@ -5,10 +5,10 @@ import datetime as dt
 class NewDoc(BaseModel):
     content: str
 
+
 class Project(BaseModel):
     project_name: str
 
+
 class Event(BaseModel):
-    event_id: str
-    timestamp : dt.datetime
-    parameters: dict = {}
+    doc: dict
