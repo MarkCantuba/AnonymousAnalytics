8dc9f6b3e6d3ff306fef2ecff512c2225c11e215
Shu Lyu <shl964@mail.usask.ca>
2020-10-13 11:48:15 -0600

    Add backend configuration to allow CORS requests

--- src/main.py
+++ src/main.py
@@ -10,6 +10,19 @@ from models import *
 
 conf = config.get()
 app = FastAPI()
+
+if conf["enable_cors"]:
+    from fastapi.middleware.cors import CORSMiddleware
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["GET", "POST", "OPTIONS", "PUT", "DELETE", "PATCH"],
+        allow_headers=["*"],
+        expose_headers=["*"]
+    )
+
 es = AsyncElasticsearch([
     {
         "host": conf["elasticsearch"]["private_ip"],
