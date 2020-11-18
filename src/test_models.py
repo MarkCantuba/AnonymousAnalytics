69dd018275a234c72b125bfde3a575421c4abef9
ask419 <ask419@usask.ca>
2020-11-18 15:02:01 -0600

    Add code to test creating a project

--- src/test_models.py
+++ src/test_models.py
@@ -1,10 +1,12 @@
 from models import Project
 
 def test_project():
+    # Create a project object
     project = Project(id='project3', name='Dare Devil2', 
     description='How to play Dare Devil2')
     
 
+    # Check the attributes to make sure a valid project is actually created
     assert type(project) == Project
     assert project.id == 'project3'
     assert project.name == 'Dare Devil2'
